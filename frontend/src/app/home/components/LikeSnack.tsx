9d459a494f337191ef518cfd3c2ac1be6d6e3653
たいせい <119865966+Taisei-Yamaguchi@users.noreply.github.com>
2024-03-18 12:27:56 -0700

    implement recommend-snack function (frontend, fullstack)

---
+++ frontend/src/app/home/components/LikeSnack.tsx
@@ -0,0 +1,60 @@
+import { useAppDispatch } from "@/store";
+import { setReloading } from "@/store/slices/reload.slice";
+import { useAppSelector } from "@/store";
+import { RootState } from "@/store";
+import { useRouter } from "next/navigation";
+import { toggleLike } from "@/django_api/snack_like";
+import { FaHeart, FaRegHeart } from "react-icons/fa"
+import React, { FC } from 'react';
+
+type Snack = {
+    id: number;
+    tid: string;
+    name: string;
+    image: string;
+    url: string;
+    price: number;
+    maker: string;
+    type: string;
+    liked: boolean;
+    like_count: number;
+    country: string;
+    account: {id:number,username:string}|null
+};
+
+type Props = {
+    snack: Snack;
+};
+
+const LikeSnack: FC<Props> =({snack}) =>{
+    const router = useRouter()
+    const dispatch = useAppDispatch()
+    const account = useAppSelector((state:RootState)=>state.loginUserSlice.account)
+	const token = useAppSelector((state:RootState)=>state.loginUserSlice.token)
+	
+    const handleLike = async () => {
+        try {
+            if (!account||!token) {
+                router.push('/login');
+                return;
+            }
+            dispatch(setReloading(true)); // reloading true
+            const data =await toggleLike(snack.id,token)
+        } catch (error) {
+            console.error('Error updating text:', error);
+        } finally {
+            dispatch(setReloading(false)); // reloading false
+        }
+    };
+    return(
+        <>
+            {/* like toggle */}
+            <button className="skeleton w-24 btn" onClick={handleLike}>
+                { snack.liked ?(<FaHeart/>):(<FaRegHeart/>)}
+                <div className="badge">{snack.like_count}</div>
+            </button>
+        </>
+    )
+}
+
+export default LikeSnack
--- venv/backend/snack/repositries/get_snacks_by_rule.py
+++ venv/backend/snack/repositries/get_snacks_by_rule.py
@@ -7,6 +7,7 @@ from django.db.models import Count
 from like.models import LikeModel
 
 def format_snack_data(snack):
+    
     return {
         'id': snack.id,
         'tid': snack.tid,
@@ -18,10 +19,11 @@ def format_snack_data(snack):
         'image': snack.image,
         'url': snack.url,
         'like_count': snack.like_count,
+        'liked': False,
         'account': {
-            'id': str(snack.account.id) if snack.account else None,
-            'username': snack.account.username if snack.account else None
-        }
+            'id': str(snack.account.id),
+            'username': snack.account.username 
+        } if snack.account else None
     }
 
 def get_popular(snacks_with_highest_likes):
@@ -46,7 +48,10 @@ def get_snacks_by_rule(rule,login_user):
             ## Get the most liked snack type by the user
             most_liked_snack_type = LikeModel.objects.filter(account_id=login_user).values('snack_id__type') \
                 .annotate(count=Count('snack_id__type')).order_by('-count').first()
+            
             # print(most_liked_snack_type["snack_id__type"])
+            if most_liked_snack_type is None:
+                return []
             snacks_with_highest_likes = list(SnackModel.objects.filter(type=most_liked_snack_type["snack_id__type"]).order_by('-like_count'))
             data = get_popular(snacks_with_highest_likes)
             return {"type":most_liked_snack_type,"items":data}
@@ -54,7 +59,8 @@ def get_snacks_by_rule(rule,login_user):
             ## Get the most liked snack country by the user
             most_liked_snack_country = LikeModel.objects.filter(account_id=login_user).values('snack_id__country') \
                 .annotate(count=Count('snack_id__country')).order_by('-count').first()
-            
+            if most_liked_snack_country is None:
+                return []
             snacks_with_highest_likes = list(SnackModel.objects.filter(country=most_liked_snack_country["snack_id__country"]).order_by('-like_count'))
             data = get_popular(snacks_with_highest_likes)
             return {"country":most_liked_snack_country,"items":data}
@@ -62,7 +68,8 @@ def get_snacks_by_rule(rule,login_user):
             ## Get the most liked snack maker by the user
             most_liked_snack_maker = LikeModel.objects.filter(account_id=login_user).values('snack_id__maker') \
                 .annotate(count=Count('snack_id__maker')).order_by('-count').first()
-            
+            if most_liked_snack_maker is None:
+                return []
             snacks_with_highest_likes = list(SnackModel.objects.filter(maker=most_liked_snack_maker["snack_id__maker"]).order_by('-like_count'))
             data = get_popular(snacks_with_highest_likes) 
             return {"maker":most_liked_snack_maker,"items":data}
