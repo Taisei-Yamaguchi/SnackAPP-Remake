b20d6674af57418fa9cb801b0568dba1c0ab84af
たいせい <119865966+Taisei-Yamaguchi@users.noreply.github.com>
2024-03-23 09:34:17 -0700

    set ALLOWED_HOST in .env

--- venv/backend/backend/settings_prod.py
+++ venv/backend/backend/settings_prod.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.getenv("ALLOWED_HOSTS")]
 
 CORS_ALLOWED_ORIGINS = [  
     'http://example.com',
