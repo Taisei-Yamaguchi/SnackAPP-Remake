5c37312706fbb3ecd89cfeeb3ed1740f02a93208
たいせい <119865966+Taisei-Yamaguchi@users.noreply.github.com>
2024-03-23 22:49:54 -0700

    set prod

--- venv/backend/backend/settings_prod.py
+++ venv/backend/backend/settings_prod.py
@@ -20,7 +20,9 @@ STATIC_ROOT="/usr/share/nginx/html/static"
 
 CORS_ALLOWED_ORIGINS = [  
     os.getenv("SERVER_ORIGIN"),
-    os.getenv("FRONTEND_ORGIN")
+    os.getenv("FRONTEND_ORGIN"),
+    'http://localhost:3000',
+    'http://35.76.4.240',
 ]
 # # Allow same origin
 # SECURE_REFERRER_POLICY = 'same-origin'
