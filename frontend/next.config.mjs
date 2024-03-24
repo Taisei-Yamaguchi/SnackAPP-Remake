c9ddb9a6e601b4420bfb15a8fed886ee167e46c8
たいせい <119865966+Taisei-Yamaguchi@users.noreply.github.com>
2024-03-23 22:31:15 -0700

    edit next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        serverActions: true,
-    },
+    // experimental: {
+    //     serverActions: true,
+    // },
     env: {
         BACKEND_ENDPOINT: process.env.BACKEND_ENDPOINT,
     },
